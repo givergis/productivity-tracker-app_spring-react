1a1bd11efd5bf0c8b21e0659f829c1fe96deee35
georgy <pkg.pess@gmail.com>
2025-06-21 14:58:17 +0400

    Initial project setup - backend + frontend

---
+++ backend/backend-productivity-tracker/src/main/java/com/givergis/backend_productivity_tracker/BackendProductivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.givergis.backend_productivity_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductivityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProductivityTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/backend-productivity-tracker/src/test/java/com/givergis/backend_productivity_tracker/BackendProductivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.givergis.backend_productivity_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProductivityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
